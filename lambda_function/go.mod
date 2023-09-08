db7647abda05465c28a2f4bb00f44b701ec6bb8b
Gildarts99 <hicks.matthew99@gmail.com>
2023-09-08 17:36:13 -0500

    function works locally

--- lambda_function/go.mod
+++ lambda_function/go.mod
@@ -2,4 +2,4 @@ module github.com/go-sqs-lambda
 
 go 1.19
 
-require github.com/aws/aws-lambda-go v1.41.0 // indirect
+require github.com/aws/aws-lambda-go v1.41.0
--- lambda_function/main.go
+++ lambda_function/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "\"Hello from Lambda!\"",
-	}
-	return response, nil
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) {
+	fmt.Println("hello world!!!")
 }
 
 func main() {
