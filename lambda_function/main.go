7dfbc407eac0b8f8596360f1e4b94eea9cae2bfd
Gildarts99 <hicks.matthew99@gmail.com>
2023-09-08 14:02:58 -0500

    updating container image

--- lambda_function/main.go
+++ lambda_function/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, event events.APIGatewayProxyRequest) {
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "\"Hello from Lambda!\"",
